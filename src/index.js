bffa50b0fb1cbfbd1b9a31f342f4e8e56e0dbe89
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-05-27 10:39:39 +0200

    Make the dropdown entries appear and disappear

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import "./modern-normalize.css";
 import "./styles.css";
+
+const dropdownElement = document.querySelector(".dropdown-element");
+
+dropdownElement.addEventListener("click", () => {
+  const entries = document.querySelectorAll(".drop-entry");
+  for (const entry of entries) {
+    entry.classList.toggle("invisible");
+  }
+});
